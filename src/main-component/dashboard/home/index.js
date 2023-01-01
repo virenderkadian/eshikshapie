8f29c40246c0d91b404c3143cbc08aead824c874
Virender Kadian <virenderkadian@Virenders-MacBook-Pro.local>
2023-01-01 21:45:08 +0530

    carousal

--- src/main-component/dashboard/home/index.js
+++ src/main-component/dashboard/home/index.js
@@ -1,14 +1,8 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/self-closing-comp */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
-import {
-  Image,
-  ImageBackground,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import {Dimen} from '../../../utils/helper';
+import {Image, ImageBackground, ScrollView, Text, View} from 'react-native';
 import Avatar from '../../../assets/avatar.png';
 import Icon from 'react-native-vector-icons/Entypo';
 import IonIcon from 'react-native-vector-icons/Ionicons';
@@ -16,66 +10,19 @@ import {Searchbar} from 'react-native-paper';
 import Filter from '../../../assets/filter.png';
 import BgImg from '../../../assets/bg.jpeg';
 import styles from './styles';
-import {FlatList} from 'react-native-gesture-handler';
 
-const CARO_DATA = [
-  {
-    text1: '40% OFF ',
-    text2: "Today's Special",
-    text3: '40%',
-    text4: 'Get a discount for every course order!',
-    text5: 'Only valid for today!',
-  },
-  {
-    text1: '40% OFF ',
-    text2: "Today's Special",
-    text3: '40%',
-    text4: 'Get a discount for every course order!',
-    text5: 'Only valid for today!',
-  },
-  {
-    text1: '40% OFF ',
-    text2: "Today's Special",
-    text3: '40%',
-    text4: 'Get a discount for every course order!',
-    text5: 'Only valid for today!',
-  },
-  {
-    text1: '40% OFF ',
-    text2: "Today's Special",
-    text3: '40%',
-    text4: 'Get a discount for every course order!',
-    text5: 'Only valid for today!',
-  },
-  {
-    text1: '40% OFF ',
-    text2: "Today's Special",
-    text3: '40%',
-    text4: 'Get a discount for every course order!',
-    text5: 'Only valid for today!',
-  },
-  {
-    text1: '40% OFF ',
-    text2: "Today's Special",
-    text3: '40%',
-    text4: 'Get a discount for every course order!',
-    text5: 'Only valid for today!',
-  },
-  {
-    text1: '40% OFF ',
-    text2: "Today's Special",
-    text3: '40%',
-    text4: 'Get a discount for every course order!',
-    text5: 'Only valid for today!',
-  },
-];
 export default function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1}}>
       <View>
-        <Header />
-        <Search />
-        <Carousal data={CARO_DATA} />
+        <ScrollView>
+          <Header />
+          <Search />
+
+          <Carousal data={CARO_DATA} />
+          <TopMentors data={MentorData} />
+          <PopularCourses data={PopularCoursesData} />
+        </ScrollView>
       </View>
     </View>
   );
@@ -165,7 +112,7 @@ const Carousal = ({data}) => {
     <>
       <ImageBackground
         resizeMode="cover"
-        style={styless.CaroItemContainerBg}
+        style={styles.CaroItemContainerBg}
         source={BgImg}>
         <ScrollView
           ref={scrollViewRef}
@@ -176,8 +123,8 @@ const Carousal = ({data}) => {
           pagingEnabled>
           {data.map((item, index) => {
             return (
-              <View key={index} style={styless.CaroItemContainer}>
-                <View style={styless.CarItemTopContainer}>
+              <View key={index} style={styles.CaroItemContainer}>
+                <View style={styles.CarItemTopContainer}>
                   <View>
                     <Text style={{color: 'white'}}>{item.text1}</Text>
                     <Text
@@ -192,7 +139,7 @@ const Carousal = ({data}) => {
                     </Text>
                   </View>
                 </View>
-                <View style={styless.CarItemBottomContainer}>
+                <View style={styles.CarItemBottomContainer}>
                   <Text style={{color: 'white'}}>{item.text4}</Text>
                   <Text style={{color: 'white'}}>{item.text5}</Text>
                 </View>
@@ -201,67 +148,229 @@ const Carousal = ({data}) => {
           })}
         </ScrollView>
       </ImageBackground>
-      <View
-        style={{
-          width: '100%',
-          marginTop: -25,
-          alignItems: 'center',
-        }}>
-        <View
-          style={{
-            borderWidth: 1,
-            justifyContent: 'center',
-            borderColor: 'red',
-            flexDirection: 'row',
-            width: '20%',
-            padding: 5,
-            borderRadius: 10,
-          }}>
+      <View style={styles.scrollIndicatorContainer}>
+        <View style={styles.scrollIndicatorContainerView}>
           {data.map((item, index) => {
             return (
               <View
                 key={index}
-                style={{
-                  width: 6,
-                  height: 6,
-                  marginHorizontal: 2,
-                  backgroundColor: 'red',
-                  borderRadius: 3,
-                }}></View>
+                style={[
+                  styles.scrollIndicatorCircle,
+                  {
+                    backgroundColor:
+                      index === currentIndex ? '#00000079' : '#00000029',
+                  },
+                ]}></View>
             );
           })}
         </View>
       </View>
-      {/* <Text style={{color: 'red'}}>
-        {currentIndex + 1} / {data.length}
-      </Text> */}
     </>
   );
 };
+const TopMentors = ({data}) => {
+  return (
+    <View style={{marginTop: 20, width: '100%'}}>
+      <View
+        style={{
+          justifyContent: 'space-between',
+          flexDirection: 'row',
+          paddingHorizontal: 20,
+        }}>
+        <Text style={{fontSize: 20, fontWeight: '500', color: 'black'}}>
+          Top Mentors
+        </Text>
+        <Text style={{fontSize: 17, fontWeight: '500', color: 'blue'}}>
+          See All
+        </Text>
+      </View>
+      <View style={{marginTop: 10}}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {data.map((item, index) => {
+            return (
+              <View
+                style={{alignItems: 'center', justifyContent: 'flex-start'}}>
+                <View
+                  style={{
+                    width: 75,
+                    height: 75,
+                    borderRadius: 37.5,
+                    marginHorizontal: 10,
+                    overflow: 'hidden',
+                  }}>
+                  <Image
+                    style={{height: 75, width: 75}}
+                    source={item.image}
+                    resizeMode="cover"
+                  />
+                </View>
+                <Text
+                  style={{
+                    fontWeight: '500',
+                    fontSize: 15,
+                    color: 'black',
+                    marginTop: 7,
+                  }}>
+                  {item.name}
+                </Text>
+              </View>
+            );
+          })}
+        </ScrollView>
+      </View>
+    </View>
+  );
+};
+const PopularCourses = ({data}) => {
+  return (
+    <View style={{marginTop: 20}}>
+      <View
+        style={{
+          justifyContent: 'space-between',
+          flexDirection: 'row',
+          paddingHorizontal: 20,
+        }}>
+        <Text style={{fontSize: 20, fontWeight: '500', color: 'black'}}>
+          Most Popular Courses
+        </Text>
+        <Text style={{fontSize: 17, fontWeight: '500', color: 'blue'}}>
+          See All
+        </Text>
+      </View>
+      <CourseList data={data} />
+    </View>
+  );
+};
 
-const styless = StyleSheet.create({
-  CaroItemContainer: {
-    height: '100%',
-    width: Dimen.Width - 40,
-    overflow: 'hidden',
-    backgroundColor: 'transparent',
+const CourseList = ({data}) => {
+  return <View></View>;
+};
+
+const CARO_DATA = [
+  {
+    text1: '40% OFF ',
+    text2: "Today's Special",
+    text3: '40%',
+    text4: 'Get a discount for every course order!',
+    text5: 'Only valid for today!',
   },
-  CaroItemContainerBg: {
-    height: Dimen.Width * 0.45,
-    width: Dimen.Width - 40,
-    marginHorizontal: 20,
-    backgroundColor: 'red',
-    overflow: 'hidden',
-    borderRadius: 30,
-    marginTop: 20,
+  {
+    text1: '40% OFF ',
+    text2: "Today's Special",
+    text3: '40%',
+    text4: 'Get a discount for every course order!',
+    text5: 'Only valid for today!',
   },
-  CarItemTopContainer: {
-    padding: 25,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+  {
+    text1: '40% OFF ',
+    text2: "Today's Special",
+    text3: '40%',
+    text4: 'Get a discount for every course order!',
+    text5: 'Only valid for today!',
   },
-  CarItemBottomContainer: {
-    paddingHorizontal: 25,
+  {
+    text1: '40% OFF ',
+    text2: "Today's Special",
+    text3: '40%',
+    text4: 'Get a discount for every course order!',
+    text5: 'Only valid for today!',
   },
-});
+];
+
+const MentorData = [
+  {id: '1', image: require('../../../assets/mentorMale01.jpg'), name: 'Jacob'},
+  {
+    id: '2',
+    image: require('../../../assets/mentorFemale01.jpg'),
+    name: 'Claire',
+  },
+  {id: '3', image: require('../../../assets/mentorMale02.jpg'), name: 'Prince'},
+  {id: '4', image: require('../../../assets/mentorMale03.jpg'), name: 'Wade'},
+  {
+    id: '5',
+    image: require('../../../assets/mentorFemale02.jpg'),
+    name: 'Katherine',
+  },
+];
+const PopularCoursesData = [
+  {
+    id: 1,
+    courseCat: '3D Design',
+    title: '3D Design Illustration',
+    price: 80,
+    discount: 40,
+    rating: 4.8,
+    image: '',
+    coursePopularity: 8298,
+  },
+  {
+    id: 2,
+    courseCat: 'Entrepeneurship',
+    title: 'Digital Entrepeneurship',
+    price: 39,
+    discountedPrice: 0,
+    rating: 4.9,
+    image: '',
+    coursePopularity: 6168,
+  },
+  {
+    id: 3,
+    courseCat: 'Business',
+    title: 'CRM Business Management',
+    price: 80,
+    discountedPrice: 48,
+    rating: 4.8,
+    image: '',
+    coursePopularity: 8298,
+  },
+  {
+    id: 4,
+    courseCat: 'UI/UX Design',
+    title: 'Learn UX Personna',
+    price: 80,
+    discountedPrice: 48,
+    rating: 4.8,
+    image: '',
+    coursePopularity: 8298,
+  },
+  {
+    id: 5,
+    courseCat: 'Programming',
+    title: 'Full Stck Web Development',
+    price: 48,
+    discountedPrice: 0,
+    rating: 4.8,
+    image: '',
+    coursePopularity: 10298,
+  },
+  {
+    id: 6,
+    courseCat: '3D Design',
+    title: '3D Characters Illustration',
+    price: 48,
+    discountedPrice: 0,
+    rating: 4.8,
+    image: '',
+    coursePopularity: 9432,
+  },
+  {
+    id: 7,
+    courseCat: 'Technology',
+    title: 'WordPress Website',
+    price: 40,
+    discountedPrice: 0,
+    rating: 4.8,
+    image: '',
+    coursePopularity: 4298,
+  },
+  {
+    id: 8,
+    courseCat: 'Programming',
+    title: 'React Native App Development',
+    price: 180,
+    discountedPrice: 40,
+    rating: 4.8,
+    image: '',
+    coursePopularity: 18298,
+  },
+];
--- src/main-component/dashboard/home/styles.js
+++ src/main-component/dashboard/home/styles.js
@@ -22,5 +22,47 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: '500',
   },
+  CaroItemContainer: {
+    height: '100%',
+    width: Dimen.Width - 40,
+    overflow: 'hidden',
+    backgroundColor: 'transparent',
+  },
+  CaroItemContainerBg: {
+    height: Dimen.Width * 0.45,
+    width: Dimen.Width - 40,
+    marginHorizontal: 20,
+    backgroundColor: 'red',
+    overflow: 'hidden',
+    borderRadius: 30,
+    marginTop: 20,
+  },
+  CarItemTopContainer: {
+    padding: 25,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  CarItemBottomContainer: {
+    paddingHorizontal: 25,
+  },
+  scrollIndicatorContainer: {
+    width: '100%',
+    marginTop: -25,
+    alignItems: 'center',
+  },
+  scrollIndicatorContainerView: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    padding: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
+  },
+  scrollIndicatorCircle: {
+    width: 6,
+    height: 6,
+    marginHorizontal: 2,
+    borderRadius: 3,
+  },
 });
 export default styles;
